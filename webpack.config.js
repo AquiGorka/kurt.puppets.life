d50ab99c122e50a125020b8dc923f90379615ecd
Gorka <gorka@aquigorka.com>
2017-11-11 08:30:18 +0100

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,33 @@
 // webpack.config.js
-var webpack = require('webpack');
+const webpack = require('webpack');
 
 // definePlugin takes raw strings and inserts them, so you can put strings of JS if you want.
-var definePlugin = new webpack.DefinePlugin({
+const definePlugin = new webpack.DefinePlugin({
   __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'true')),
-  __PROD__: JSON.stringify(JSON.parse(process.env.BUILD_PROD || 'false'))
-});
+  __PROD__: JSON.stringify(JSON.parse(process.env.BUILD_PROD || 'false')),
+  GOOGLE_APIKEY: JSON.stringify(process.env.GOOGLE_APIKEY)
+})
 
 module.exports = {
   entry: [
-	'./src/index.js'
+  './src/index.js'
   ],
   output: {
-  	path: './public/js/',
+    path: './public/js/',
     filename: 'index.js'       
   },
   plugins: [
-  	definePlugin
+    definePlugin
   ],
   module: {
     loaders: [
-      { test: /.js$/, loader: 'babel-loader', exclude: /node_modules/, query: { presets:['react', 'es2015'] } }
+      { test: /.js$/, loader: 'babel-loader', exclude: /node_modules/, query: { presets:['react', 'es2015'] } },
+      { test: /\.json$/, loader: 'json-loader' }
     ]
+  },
+  node: {
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty'
   }
-};
+}
